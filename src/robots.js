4e8e607acdc3b6bdffd0106ee70ffdba58f59b4d
Joseph Doughty <josahty@gmail.com>
2019-01-16 10:04:05 -0500

    created Card component, Robots file, and added 3 cards to page

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Michael Scott',
+        username: 'mscott',
+        email: 'mscott@dundermifflin.com'
+    },
+    {
+        id: 2,
+        name: 'Dwight Schrute',
+        username: 'dschrute',
+        email: 'dschrute@dundermifflin.com'
+    },
+    {
+        id: 3,
+        name: 'Jim Halpert',
+        username: 'jhalpert',
+        email: 'jhalpert@dundermifflin.com'
+    },
+    {
+        id: 4,
+        name: 'Pam Beesley',
+        username: 'pbeesley',
+        email: 'pbeesley@dundermifflin.com'
+    },
+    {
+        id: 5,
+        name: 'Jan Levinson',
+        username: 'jlevinson',
+        email: 'jlevinson@dundermifflin.com'
+    },
+    {
+        id: 6,
+        name: 'Andy Bernard',
+        username: 'abernard',
+        email: 'abernard@dundermifflin.com'
+    },
+    {
+        id: 7,
+        name: 'Kevin Malone',
+        username: 'kmalone',
+        email: 'kmalone@dundermifflin.com'
+    },
+    {
+        id: 8,
+        name: 'Angela Martin',
+        username: 'amartin',
+        email: 'amartin@dundermifflin.com'
+    }
+];
