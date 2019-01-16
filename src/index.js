6133caa37776ba65546a3fb0d4d967c7e9ee9acf
Joseph Doughty <josahty@gmail.com>
2019-01-16 10:27:02 -0500

    added CardList to allow cleaner display of cards on the page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons'
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import { robots } from './robots';
 import * as serviceWorker from './serviceWorker';
 
@@ -10,16 +10,7 @@ import * as serviceWorker from './serviceWorker';
 //greeting is a prop (property)
 // ReactDOM.render(<Card />, document.getElementById('root'));
 ReactDOM.render(
-                <div>
-                    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} username={robots[0].username}/>
-                    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} username={robots[1].username}/>
-                    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} username={robots[2].username}/>
-                    {/* <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} username={robots[3].username}/>
-                    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email} username={robots[4].username}/>
-                    <Card id={robots[5].id} name={robots[5].name} email={robots[5].email} username={robots[5].username}/>
-                    <Card id={robots[6].id} name={robots[6].name} email={robots[6].email} username={robots[6].username}/>
-                    <Card id={robots[7].id} name={robots[7].name} email={robots[7].email} username={robots[7].username}/> */}
-                </div>
+                <CardList robots={robots} />                
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
