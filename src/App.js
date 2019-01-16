b56470b3769912930d46158c7ad944319db59c44
Joseph Doughty <josahty@gmail.com>
2019-01-16 11:05:51 -0500

    added a functioning search box using state

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
+
+//state can change based on what is in search box
+//state >> props
+class App extends Component {
+    constructor() {
+        super();
+        //this state describes the app and lives in the parent component
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    //on search change, set searchfield to the value in the search box
+    onSearchChange = (e) => {
+        this.setState({ searchfield: e.target.value })
+    }
+
+    render() {
+        //create list of robots whose name contains what has been typed in the search box
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        //render the component
+        return (
+            <div className='tc'>
+                <h1>Robot Office Directory</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots = {filteredRobots}/>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons'
 import './index.css';
-import CardList from './CardList';
-import { robots } from './robots';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 //"this whole thing is what React does"
 //greeting is a prop (property)
 // ReactDOM.render(<Card />, document.getElementById('root'));
-ReactDOM.render(
-                <CardList robots={robots} />                
-, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
